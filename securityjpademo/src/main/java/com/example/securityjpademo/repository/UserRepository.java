56dcd0494b24be1ca86f6122c22c0b594582117f
“sheruidan” <sheruidan@czyugang.com>
2019-06-05 16:46:35 +0800

    no message

--- securityjpademo/src/main/java/com/example/securityjpademo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.securityjpademo.repository;
-
-import com.example.securityjpademo.pojo.FKUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<FKUser, Long> {
-    FKUser findByLoginName(String loginName);
-}
